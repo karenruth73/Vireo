1103cb939531381aa3282b4df99d1bf2e5e331e7
William Welling <wwelling@library.tamu.edu>
2017-07-19 16:48:23 -0500

    added tomcat container factory for max swallow size configuration

--- src/main/java/org/tdl/vireo/config/AppWebMvcConfig.java
+++ src/main/java/org/tdl/vireo/config/AppWebMvcConfig.java
@@ -1,6 +1,11 @@
 package org.tdl.vireo.config;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -28,6 +33,18 @@ public class AppWebMvcConfig extends CoreWebMvcConfig {
         registry.addResourceHandler("/public/**").addResourceLocations("file:" + Application.BASE_PATH + "public/");
 
         registry.setOrder(Integer.MAX_VALUE - 2);
+    }
+
+    @Bean
+    public TomcatEmbeddedServletContainerFactory containerFactory() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+        return factory;
     }
 
 }
